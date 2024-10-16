260d72f27b5bcd42a1014dfe1faac30111724a94
jisun.park <pjs981114@naver.com>
2024-10-17 00:20:06 +0900

    feat: CORS add

--- src/main/java/com/signal/global/config/SecurityConfig.java
+++ src/main/java/com/signal/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.signal.global.config;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -38,5 +43,34 @@ public class SecurityConfig {
         );
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource () {
+        CorsConfiguration configuration = new CorsConfiguration();
+        List<String> allowed =
+            Arrays.asList(
+                "http://localhost:8080",
+                "http://localhost:3000"
+            );
+        configuration.setAllowedOrigins(allowed);
+        configuration.setAllowedMethods(
+            Arrays.asList("GET", "Post", "PUT", "Delete", "PATCH", "OPTIONS")
+        );
+        configuration.setAllowedHeaders(
+            Arrays.asList(
+                "Authorization", "Cache-Control", "Content-Type"
+            )
+        );
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        configuration.setExposedHeaders(Arrays.asList(
+            "Authorization", "Cache-Control", "Content-Type"
+        ));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
