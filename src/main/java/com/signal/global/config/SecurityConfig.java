6e5a4039ba18698722236f599edbaaf3d91afb14
rkdgu <rkdgu@HM>
2024-10-21 17:40:25 +0900

    ??

--- src/main/java/com/signal/global/config/SecurityConfig.java
+++ src/main/java/com/signal/global/config/SecurityConfig.java
@@ -54,8 +54,8 @@ public class SecurityConfig {
 
     	http
     	.formLogin((auth)->auth
-//    			.loginPage("/login") // 프론트 엔드 주소값
-    			.loginProcessingUrl("/login") // 프론트 폼 액션값이랑 일치해야함
+    			.loginPage("/Login") // 프론트 엔드 주소값
+    			.loginProcessingUrl("http://localhost:8080/login") // 프론트 폼 액션값이랑 일치해야함
     			.permitAll());
 
     	http
